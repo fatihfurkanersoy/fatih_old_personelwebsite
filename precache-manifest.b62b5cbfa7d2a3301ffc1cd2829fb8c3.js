5c44a2492f2aeba29318edc2b9fe88a9035a92d2
Fatih Furkan-Ubuntu <ersoy279@gmail.com>
2021-11-01 19:05:33 +0300

    Updates

---
+++ precache-manifest.b62b5cbfa7d2a3301ffc1cd2829fb8c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053cf41c9e6923acd293779ff767d007",
+    "url": "/fatihfurkanersoy/index.html"
+  },
+  {
+    "revision": "f48f4c412c833ed382b3",
+    "url": "/fatihfurkanersoy/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "c73c04fa3b19b32d2148",
+    "url": "/fatihfurkanersoy/static/js/2.6d09358a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/fatihfurkanersoy/static/js/2.6d09358a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48f4c412c833ed382b3",
+    "url": "/fatihfurkanersoy/static/js/main.5db19782.chunk.js"
+  },
+  {
+    "revision": "70b069453a980387b2f4",
+    "url": "/fatihfurkanersoy/static/js/runtime-main.93645b08.js"
+  },
+  {
+    "revision": "2d46bd7eb4b58bf633778d8e90af679b",
+    "url": "/fatihfurkanersoy/static/media/benoda.2d46bd7e.png"
+  },
+  {
+    "revision": "bf3142da583e200486d479bf8e5ef11c",
+    "url": "/fatihfurkanersoy/static/media/elektrik.bf3142da.png"
+  },
+  {
+    "revision": "fe062533b935199c136034161a111073",
+    "url": "/fatihfurkanersoy/static/media/fatih_2.fe062533.png"
+  },
+  {
+    "revision": "1484f979fc870db71fa5580bf5376b7a",
+    "url": "/fatihfurkanersoy/static/media/kutarson.1484f979.jpg"
+  },
+  {
+    "revision": "1c09596d6c65f5bfe18f7045bab99090",
+    "url": "/fatihfurkanersoy/static/media/resume.1c09596d.pdf"
+  },
+  {
+    "revision": "b1870f569beb87d31c452314609fbecf",
+    "url": "/fatihfurkanersoy/static/media/tcdd.b1870f56.png"
+  },
+  {
+    "revision": "c58252540844f4c5e2c4b12a346c8205",
+    "url": "/fatihfurkanersoy/static/media/yazigt.c5825254.png"
+  }
+]);
